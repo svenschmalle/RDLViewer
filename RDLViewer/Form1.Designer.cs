d09b29793b6ab00b030dae743040c9f6669818a8
Sven <sven.schmalle@ta-logistic.de>
2016-07-12 13:33:27 +0200

    RDL-Viewer Dateien hinzugefügt

---
+++ RDLViewer/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace RDLViewer
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Erforderliche Designervariable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Verwendete Ressourcen bereinigen.
+		/// </summary>
+		/// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Vom Windows Form-Designer generierter Code
+
+		/// <summary>
+		/// Erforderliche Methode für die Designerunterstützung.
+		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.Size = new System.Drawing.Size(727, 844);
+            this.reportViewer1.TabIndex = 2;
+            this.reportViewer1.ZoomMode = Microsoft.Reporting.WinForms.ZoomMode.PageWidth;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 844);
+            this.Controls.Add(this.reportViewer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Report Viewer";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+    }
+}
+
---
+++ RDLViewer/Form1.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RDLViewer
+{
+	public partial class Form1 : Form
+	{
+        private string[] _args;
+        public Form1(string[] args)
+		{
+            InitializeComponent();
+            _args = args;
+		}
+
+        private void Form1_Load(object sender, EventArgs e)
+		{
+            if (_args.Length == 0)
+            {
+                MessageBox.Show("Kein Druckbericht angegeben","Fehler!",MessageBoxButtons.OK,MessageBoxIcon.Stop);
+                Application.Exit();
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Normal;
+                DataSet ds = new DataSet();
+                ds.ReadXml(_args[0]);
+                ReportDataSource rds = new ReportDataSource("Kopfdaten", ds.Tables["Kopfdaten"]);
+                ReportDataSource rdsPosten = new ReportDataSource("Posten", ds.Tables["Posten"]);
+                this.reportViewer1.LocalReport.DataSources.Add(rds);
+                this.reportViewer1.LocalReport.DataSources.Add(rdsPosten);
+                this.reportViewer1.SetDisplayMode(DisplayMode.PrintLayout);
+                this.reportViewer1.LocalReport.ReportPath = Path.Combine(Application.StartupPath, "Reports", Path.GetFileNameWithoutExtension(_args[0]) + ".rdl");
+                this.reportViewer1.RefreshReport();
+            }
+        }
+
+        private void reportViewer1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
